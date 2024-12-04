42c1e178d4ce2f0f20c67a71c79bfa0ad4205604
avallenn <avallen@syr.edu>
2024-12-03 19:43:05 -0500

    part 2

--- code/location_dashboard.py
+++ code/location_dashboard.py
@@ -5,4 +5,41 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import streamlit_folium as sf
+import folium
 st.set_page_config(layout="wide")
+
+df = pd.read_csv('./cache/tickets_in_top_locations.csv')
+st.title('Top Locations for Parking Tickets within Syracuse')
+#st.dataframe(df)
+locations = df['location'].unique()
+new_location = st.selectbox('Select a location', locations)
+
+total_issued = df[df['location'] == new_location].count()
+# add up total amount for the specified location from the selectbox
+total_amount = df[df['location'] == new_location]['amount'].sum()
+st.write(f'Total Amount: {total_amount}')
+st.write(f'Total Issued Tickets: {total_issued[0]}')
+
+col1, col2 = st.columns(2)
+
+with col1:
+    #Bar Chart: Show the distribution of tickets by day of the week
+    fig1, ax1 = plt.subplots()
+    ax1.set_title('Tickets Issued by Day of Week')
+    sns.countplot(data=df[df['location'] == new_location], x='dayofweek', ax=ax1, hue = 'dayofweek')
+    st.pyplot(fig1)
+
+with col2:
+    #Line Chart: Show the distribution of tickets by hour of the da
+    fig2, ax2 = plt.subplots()
+    ax2.set_title('Tickets Issued by Hour of Day')
+    sns.countplot(data=df[df['location'] == new_location], x='hourofday', ax=ax2, hue = 'hourofday')
+    st.pyplot(fig2)
+
+lat = df[df['location'] == new_location]['lat'].values[0]
+lon = df[df['location'] == new_location]['lon'].values[0]
+location = (lat, lon)
+map = folium.Map(location=location, zoom_start=14)
+new_map = folium.Marker(location=location, popup=new_location).add_to(map)
+sf.folium_static(map)
--- code/map_dashboard.py
+++ code/map_dashboard.py
@@ -12,3 +12,14 @@ CUSE = (43.0481, -76.1474)  # center of map
 ZOOM = 14                   # zoom level
 VMIN = 1000                 # min value for color scale
 VMAX = 5000                 # max value for color scale
+
+df = pd.read_csv('./cache/top_locations_mappable.csv')
+#st.dataframe(df)
+
+st.title('Top Locations for Parking Tickets within Syracuse')
+geo_df = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.lon, df.lat))
+#st.write(geo_df), just adds a geomertry column to the dataframe
+map = folium.Map(location=CUSE, zoom_start=ZOOM)
+#sf.folium_static(map), prints out the map but does not add any tags
+cuse_map = geo_df.explore(geo_df['amount'], m=map, vmin=VMIN, vmax=VMAX, legend=True, legend_name='Amount', marker_kwds = {"radius": 10, "fill": True})
+sf.folium_static(cuse_map)
