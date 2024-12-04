e9e372f46d87c9976dcffdeee76ff6d73cd93efb
avallenn <avallen@syr.edu>
2024-12-04 15:23:03 -0500

    changes

--- code/etl.py
+++ code/etl.py
@@ -5,19 +5,23 @@ import streamlit as st
 def top_locations(violations_df : pd.DataFrame, threshold=1000) -> pd.DataFrame:
     #locations of parking tickets with $1,000 r more total aggregted violation amounts
     totalamount_df = violations_df.pivot_table(index='location', values='amount', aggfunc='sum').sort_values(by='amount', ascending=False)
-    totalamount_df['location'] = totalamount_df.index
-    totalamount_df.reset_index(drop=True, inplace=True)
-    return totalamount_df[totalamount_df['amount'] >= threshold]
+    top_df = totalamount_df[totalamount_df['amount'] >= threshold]
+    top_df = top_df.sort_values(by='amount', ascending=False)
+    top_df['location'] = top_df.index
+    top_df = top_df.reset_index(drop=True)
+    return top_df
 
 def top_locations_mappable(violations_df : pd.DataFrame, threshold=1000) -> pd.DataFrame:
     top_map = top_locations(violations_df, threshold)
-    location_map = violations_df[['location', 'lat', 'lon']].drop_duplicates(subset=['location'])
-    merged_df = pd.merge(top_map, location_map, on='location')
-    return merged_df
+    merged_df = pd.merge(top_map, violations_df, on='location')
+    top_loc_df = merged_df[['location', 'amount_x', 'lat', 'lon']]
+    top_loc_dedupe_df = top_loc_df.drop_duplicates(subset='location')
+    return top_loc_dedupe_df
 
 
 def tickets_in_top_locations(violations_df : pd.DataFrame, threshold=1000)  -> pd.DataFrame:
-    top_locations_df = top_locations(violations_df)
+    top_locations_df = top_locations(violations_df, threshold)
+    del top_locations_df['amount']
     merged_df = pd.merge(top_locations_df[['location']], violations_df, on='location')
     return merged_df
 
